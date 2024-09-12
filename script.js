bf5a3937c3541f7ade62539b13e414fa7ef72552
huelden <156624639+huelden@users.noreply.github.com>
2024-09-12 21:54:53 +0100

    seguindo js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ const addressWarn = document.getElementById("address-warn");
 cartBtn.addEventListener("click", function () {
   // <- Corrigido com parêntese correto
   cartModal.style.display = "flex";
-}); */
+}); 
 
 document.addEventListener("DOMContentLoaded", function () {
   const menu = document.getElementById("menu");
@@ -40,3 +40,34 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 });
+
+*/
+const menu = document.getElementById("menu")
+const cartBtn = document.getElementById("cart-btn")
+const cartModal = document.getElementById("cart-modal")
+const cartItemsContainer = document.getElementById("cart-items")
+const cartTotal = document.getElementById("cart-total")
+const checkoutBtn =document.getElementById("checkout-btn")
+const closeModalBtn = document.getElementById("close-modal-btn")
+const cartCounter = document.getElementById("cart-count")
+const addressInput = document.getElementById("address")
+const addressWarn = document.getElementById("address-warn")
+
+cartBtn.addEventListener("click", function(){
+  cartModal.style.display = "flex"
+})
+
+cartModal.addEventListener("click", function(event){
+  if(event.target === cartModal){
+    cartModal.style.display = "none"
+  }
+})
+
+closeModalBtn.addEventListener("click", function(){
+  cartModal.style.display = "none"
+})
+
+
+menu.addEventListener("click", function(event){
+  console.log(event.target)
+})
